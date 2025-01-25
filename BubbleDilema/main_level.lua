a1d28f1af8262f76e57306578a2c0ec50bbe9a68
Filip <filip.lipovac@gmail.com>
2025-01-25 20:03:31 +0100

    refactor for level number

--- BubbleDilema/main_level.lua
+++ BubbleDilema/main_level.lua
@@ -20,12 +20,16 @@ local bubbleMinSize = screenW / 6
 local bubbleGenerationBeginY = screenH + bubbleMaxSize
 local bubbleFinishY = -50
 
+local bubbleVersions = {"v1", "v2", "v3"}
 local bubbleColors = {"red", "blue", "orange", "green", "purple"}
-local bubbleColorScore = {}
-local colorTargets = {}
+
+local finishedBubblesScore = {}
+local poppedBubblesScore = {}
+local levelTarget = {}
 
 local scorePlaceholder = nil
 local goalPlaceholder = nil
+local finishDetector = nil
 
 local bubbles = {}
 
@@ -33,37 +37,27 @@ local bubbles = {}
 -- populate values
 --------------------------------------------------------------------------------------------------------------
 
-local function populateColorScore()
+local function populateBubbleScore(_level)
     for i, color in pairs(bubbleColors) do
-        bubbleColorScore[color] = 0
+        finishedBubblesScore[color] = 0
+        poppedBubblesScore[color] = 0
     end
 end
 
-local function populateInitValues()
-    populateColorScore()
+local function populateInitValues(_level)
+    populateBubbleScore(_level)
 end
 
 --------------------------------------------------------------------------------------------------------------
 -- bubble mechanics
 --------------------------------------------------------------------------------------------------------------
 
-local function bustBubble(_bubble)
-    transition.to(
-        _bubble, {
-            time=50, 
-            delay=0,
-            alpha=0,
-            width=_bubble.width * 1.1,
-            height=_bubble.height * 1.1,
-            onComplete = function()
-                if _bubble then
-                    _bubble:removeSelf()
-                end
-            end
-        }
-    )
+local function popBubble(_bubbleImage)
+    if poppedBubblesScore[_bubbleImage.colorTag] then
+        poppedBubblesScore[_bubbleImage.colorTag] = poppedBubblesScore[_bubbleImage.colorTag] + 1
+        print ("Popped " .. _bubbleImage.colorTag .. " bubbles: " ..  poppedBubblesScore[_bubbleImage.colorTag])
+    end
 end
-
 
 --------------------------------------------------------------------------------------------------------------
 -- event listeners
@@ -71,7 +65,22 @@ end
 
 local function onBubbleTap( event )
     if ( event.phase == "began" ) then
-        bustBubble(event.target)
+        popBubble(event.target)
+
+        transition.to(
+            event.target, {
+                time=50, 
+                delay=0,
+                alpha=0,
+                width=event.target.width * 1.1,
+                height=event.target.height * 1.1,
+                onComplete = function()
+                    if event.target then
+                        event.target:removeSelf()
+                    end
+                end
+            }
+        )
     end
 
     return true
@@ -79,9 +88,9 @@ end
 
 local function onBubbleFinishCollision( self, event )
     if event.other ~= nil then
-        if bubbleColorScore[event.other.colorTag] ~= nil then
-            print ("Color ".. event.other.colorTag .. ", count: " .. bubbleColorScore[event.other.colorTag] )
-            bubbleColorScore[event.other.colorTag] = bubbleColorScore[event.other.colorTag] + 1
+        if finishedBubblesScore[event.other.colorTag] ~= nil then
+            print ("Color ".. event.other.colorTag .. ", count: " .. finishedBubblesScore[event.other.colorTag] )
+            finishedBubblesScore[event.other.colorTag] = finishedBubblesScore[event.other.colorTag] + 1
         end
 
         event.other:removeSelf()
@@ -96,8 +105,8 @@ local function createNextBubble()
     -- todo: ovo ovisi o trenutnom statusu igre, dodaju se osnovni podaci za kreiranje bubble-a - tip, boja, size
     local bubble = {}
 
-    bubble.imgVersion = "v1"
-    bubble.color = "blue"
+    bubble.imgVersion = bubbleVersions[math.random(1, 3)]
+    bubble.color =  bubbleColors[math.random(1, 5)]
     bubble.size = math.random(bubbleMinSize, bubbleMaxSize)
     bubble.evil = false
 
@@ -144,80 +153,61 @@ end
 -- scene setup
 --------------------------------------------------------------------------------------------------------------
 
-local function showBubbleColorScore()
-    if #bubbleColorScore > 0 then
-        for color, score in pairs(bubbleColorScore) do
+local function showfinishedBubblesScore()
+    if #finishedBubblesScore > 0 then
+        for color, score in pairs(finishedBubblesScore) do
             display.newText( options )
         end
     end
 end
 
-local function scaleScorePlaceholder()
-    if scorePlaceholder ~= nil then 
-        -- scorePlaceholder 
+local function createSceneWalls(_level)
+    if _level == 1 then
+        leftSideWall = display.newRect(-bubbleMinSize/2, 0, bubbleMinSize, screenH )
+        rightSideWall = display.newRect(screenW + bubbleMinSize/2, 0, bubbleMinSize, screenH)
+    
+        leftSideWall.anchorX = 1
+        leftSideWall.anchorY = 0
+        rightSideWall.anchorX = 0
+        rightSideWall.anchorY = 0
+        leftSideWall:setFillColor(1, 0, 0, 0)
+        rightSideWall:setFillColor(1, 1, 0, 0)
+    
+        physics.addBody( leftSideWall, "static")
+        physics.addBody( rightSideWall, "static")
     end
 end
 
-local function createSceneInvisibleWalls()
-    -- leftSideWall = display.newRect(- bubbleMaxSize * 2, bubbleGenerationBeginY, bubbleMaxSize, bubbleGenerationBeginY - bubbleFinishY)
-    -- rightSideWall = display.newRect(screenW + bubbleMaxSize, bubbleGenerationBeginY, bubbleMaxSize, bubbleGenerationBeginY - bubbleFinishY)
-    leftSideWall = display.newRect(-bubbleMinSize/2, 0, bubbleMinSize, screenH )
-    rightSideWall = display.newRect(screenW + bubbleMinSize/2, 0, bubbleMinSize, screenH)
-
-    leftSideWall.anchorX = 1
-    leftSideWall.anchorY = 0
-    rightSideWall.anchorX = 0
-    rightSideWall.anchorY = 0
-    leftSideWall:setFillColor(1, 0, 0, 0)
-    rightSideWall:setFillColor(1, 1, 0, 0)
-
-    physics.addBody( leftSideWall, "static")
-    physics.addBody( rightSideWall, "static")
-
+local function createFinishDetector(_level)
     -- collision top wall
-    topWall = display.newRect(0, 0, screenW, bubbleMinSize / 2)
-    topWall.anchorX = 0
-    topWall.anchorY = 0
+    finishDetector = display.newRect(0, 0, screenW, bubbleMinSize / 2)
+    finishDetector.anchorX = 0
+    finishDetector.anchorY = 0
 
-    topWall:setFillColor(0, 0, 1, 1)
+    finishDetector:setFillColor(0, 0, 1, 1)
 
-    physics.addBody( topWall, "static", { density=1.0, friction=1, bounce=0.01 , isSensor = true})
+    physics.addBody( finishDetector, "static", { density=1.0, friction=1, bounce=0.01 , isSensor = true})
 
-    topWall.collision = onBubbleFinishCollision
-    topWall:addEventListener( "collision" )
+    finishDetector.collision = onBubbleFinishCollision
+    finishDetector:addEventListener( "collision" )
 end
 
-local function createTopBar()
+local function setLevelTarget(_level)
+    if _level == 1 then 
+        levelTarget["blue"] = 20
+    end
 end
 
--- local function createScorePlaceholder()
---     scorePlaceholder = display.newRect(0, 0, screenW / 3, 100)
---     scorePlaceholder.anchorX = 0
---     scorePlaceholder.anchorY = 0
---     scorePlaceholder:setFillColor(0.9, 0.9, 0.9, 1)
--- end
+local function setupLevel(_level)
+    setLevelTarget(_level)
 
--- local function createGoalPlaceholder()
+    populateInitValues(_level)
 
---     goalPlaceHolder = display.newRect(screenH, 0, screenW / 3, 100)
---     goalPlaceHolder.anchorX = 1
---     goalPlaceHolder.anchorY = 0
---     goalPlaceHolder:setFillColor(1, 0.1, 0, 1)
--- end
-
-local function setupLevel()
-    physics.start()
-    physics.setGravity( 0, -5)
-
-    populateInitValues()
-
-    createSceneInvisibleWalls()
-
-    createTopBar()
+    createSceneWalls(_level)
+    createFinishDetector(_level)
 
     scorePlaceholder = helper.createScorePlaceholder(display, screenH, screenW)
     goalPlaceHolder = helper.createGoalPlaceholder(display, screenH, screenW)
-
 end
 
 -- local function gameLoop()
@@ -245,11 +235,14 @@ function scene:show( event )
 		-- Called when the scene is still off screen and is about to move on screen
 	elseif phase == "did" then
 		-- Called when the scene is now on screen
-        setupLevel()
+
+        physics.start()
+        physics.setGravity( 0, -5)
+
+        setupLevel(1)
 
         timer.performWithDelay(500, createBubble ,0)
     end
-
 end
 
 function scene:hide( event )
