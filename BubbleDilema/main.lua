c0ea5b00d916abd4f6f5767ed10745e012b03ea8
MateaIg <matea.ignatoski@gmail.com>
2025-01-25 13:57:30 +0100

    Add basic menu

--- BubbleDilema/main.lua
+++ BubbleDilema/main.lua
@@ -4,11 +4,14 @@
 ---
 -----------------------------------------------------------------------------------------
 
--- hide the status bar
 display.setStatusBar( display.HiddenStatusBar )
+
+math.randomseed(os.time())
+
+introSound = audio.loadStream( "res/audio/map-background-music-v1.wav" )
 
 -- include the Corona "composer" module
 local composer = require "composer"
 
 -- load menu screen
-composer.gotoScene( "main_level" )
+composer.gotoScene( "menu" )
---
+++ BubbleDilema/menu.lua
@@ -0,0 +1,91 @@
+-----------------------------------------------------------------------------------------
+--
+-- menu.lua
+--
+-----------------------------------------------------------------------------------------
+
+local composer = require("composer")
+local scene = composer.newScene()
+
+--------------------------------------------
+local widget = require "widget"
+
+local screenW, screenH, halfW = display.actualContentWidth, display.actualContentHeight, display.contentCenterX
+
+function scene:create(event)
+    local sceneGroup = self.view
+
+    -- Add a background image
+    local background = display.newImageRect(sceneGroup, "res/img/bubble_background.jpg", screenW, screenH)
+    background.x = display.contentCenterX
+    background.y = display.contentCenterY
+
+    -- Create a play button
+    local playButton = widget.newButton({
+        label = "Play",
+        fontSize = 24,
+        shape = "roundedRect",
+        width = 200,
+        height = 50,
+        cornerRadius = 10,
+        fillColor = { default = { 0, 0.6, 1 }, over = { 0, 0.4, 0.8 } },
+        labelColor = { default = { 1, 1, 1 }, over = { 0.8, 0.8, 0.8 } },
+        onRelease = function()
+            composer.gotoScene("main_level")
+        end,
+    })
+    playButton.x = display.contentCenterX
+    playButton.y = screenH - 100 
+    sceneGroup:insert(playButton)
+end
+
+function scene:show(event)
+    local sceneGroup = self.view
+    local phase = event.phase
+
+    if phase == "will" then
+        -- Called when the scene is still off screen and is about to move on screen
+    elseif phase == "did" then
+        -- Called when the scene is now on screen
+        -- Play background music
+        audio.play(introSound, {
+            channel = 1,
+            loops = -1
+        }) -- Infinite loop for background music
+    end
+end
+
+function scene:hide(event)
+    local sceneGroup = self.view
+    local phase = event.phase
+
+    if phase == "will" then
+        -- Called when the scene is on screen and is about to move off screen
+        -- Stop audio when leaving the scene
+        audio.fadeOut( 1, 200 )
+    elseif phase == "did" then
+        -- Called when the scene is now off screen
+    end
+end
+
+function scene:destroy(event)
+    local sceneGroup = self.view
+
+    -- Remove and dispose of the audio when the scene is destroyed
+    if introSound then
+        audio.dispose(introSound)
+        introSound = nil
+    end
+end
+
+---------------------------------------------------------------------------------
+
+-- Listener setup
+scene:addEventListener("create", scene)
+scene:addEventListener("show", scene)
+scene:addEventListener("hide", scene)
+scene:addEventListener("destroy", scene)
+
+-----------------------------------------------------------------------------------------
+
+return scene
