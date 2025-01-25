eb0a97ce10010874f446987e8fb344172c7e7274
Filip <filip.lipovac@gmail.com>
2025-01-25 13:45:55 +0100

    bubble timer gen i pop

--- BubbleDilema/main_level.lua
+++ BubbleDilema/main_level.lua
@@ -8,58 +8,85 @@
 
 local composer = require( "composer" )
 local math = require("math")
-
-
-math.randomseed(os.time())
+local physics = require("physics")
 
 local scene = composer.newScene()
 local sceneGroup = {}
 local screenW, screenH, halfW = display.actualContentWidth, display.actualContentHeight, display.contentCenterX
 
-local object = display.newImage( "sprites/bubble_transparent_placeholder.png")
-object.name = "ball object"
+math.randomseed(os.time())
+display.setDefault( "background", 1,1,1)
 
-object.width = 200
-object.height = 200
-object.x = halfW
-object.y = screenH
+bubbles = {}
 
-local function onObjectTap( event )
-    print( "Tap bubble x:" .. event.target.x .. "y: " .. event.target.y)
-    
-    local popImage = display.newImage( "sprites/splatter_pop.png")
 
-    popImage.width = 200
-    popImage.height = 200
-    popImage.x = event.target.x;
-    popImage.y = event.target.y;
+local function showBubbleBurst(_bubble)
+    local popImage = display.newImageRect( "sprites/splatter_pop.png", _bubble.width, _bubble.height)
+    popImage.alpha= 0.6
+    popImage.x = _bubble.x;
+    popImage.y = _bubble.y;
 
     transition.to(
         popImage, {
-            time=500, 
-            delay=2000,
+            time=400, 
+            delay=100,
             alpha=0,
             onComplete = function()
-                popImage = nil
+                display.remove(popImage)
             end
         }
     )
+end
 
-    event.target.x = math.random(0, screenW)
-    event.target.y = screenH
+--------------------------------------------------------------------------------------------------------------
+-- event listeners
+--------------------------------------------------------------------------------------------------------------
+
+local function onBubbleTap( event )
+    print( "Tap bubble x:" .. event.target.width .. "y: " .. event.target.height)
+    showBubbleBurst(event.target)
+
+    event.target:removeSelf()
+
+    return true
 end
 
 
-local function gameLoop()
-    if object.y ~= nil then
-        object.y = object.y - 2;
-        if object.y < 0 then 
-            object.y = screenH;
-        end
-    end
-end
+local function createBubble()
+    local bubble = {}
+    bubble.image = display.newImageRect( "sprites/bubble_transparent_placeholder.png", 200, 200)
+    bubble.name = "ball bubble"
 
---- scene interface funcitons
+    bubble.image.x = math.random(0, screenW)
+    bubble.image.y = screenH
+
+    print( "Bubble created!" )
+    physics.addBody( bubble.image , "dynamic", { radius=bubble.image.width/2, density=1.0, friction=0.3, bounce=0.2 })
+ 
+    bubble.image:addEventListener( "tap", onBubbleTap)
+
+    table.insert(bubble, bubbles)
+end
+--------------------------------------------------------------------------------------------------------------
+-- game loop
+--------------------------------------------------------------------------------------------------------------
+
+-- local function gameLoop()
+--     if object.y ~= nil then
+--         object.y = object.y - 2;
+--         if object.y < 0 then 
+--             object.y = screenH;
+--         end
+--     end
+-- end
+
+--------------------------------------------------------------------------------------------------------------
+-- scene interface funcitons
+--------------------------------------------------------------------------------------------------------------
+
+function scene:create( event )
+	sceneGroup = self.view
+end
 
 function scene:show( event )
 	sceneGroup = self.view
@@ -69,7 +96,12 @@ function scene:show( event )
 		-- Called when the scene is still off screen and is about to move on screen
 	elseif phase == "did" then
 		-- Called when the scene is now on screen
-	end
+        physics.start()
+        physics.setGravity( 0, -2)
+
+        timer.performWithDelay(500, createBubble ,0)
+    end
+
 end
 
 function scene:hide( event )
@@ -97,8 +129,14 @@ function scene:destroy( event )
 	sceneGroup = self.view
 end
 
+--------------------------------------------------------------------------------------------------------------
+-- registered listeners
+--------------------------------------------------------------------------------------------------------------
 
-Runtime:addEventListener("enterFrame", gameLoop)
-object:addEventListener( "tap", onObjectTap )
+scene:addEventListener("create", scene)
+scene:addEventListener("show", scene)
+scene:addEventListener("hide", scene)
+scene:addEventListener("destroy", scene)
+-- Runtime:addEventListener("enterFrame", gameLoop)
 
 return scene
