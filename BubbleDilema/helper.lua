7ce5849b7b40e4a45fccede1d375c0be6c18a7f7
Filip <filip.lipovac@gmail.com>
2025-01-25 20:02:05 +0100

    placeholder color and update function init

--- BubbleDilema/helper.lua
+++ BubbleDilema/helper.lua
@@ -4,7 +4,7 @@ function createGoalPlaceholder(_display, _screenH, _screenW)
     placeholder = _display.newRect(_screenW, 0, _screenW / 3, 100)
     placeholder.anchorX = 1
     placeholder.anchorY = 0
-    placeholder:setFillColor(0.3, 0.1, 0.1, 1)
+    placeholder:setFillColor(0.9,0.9,0.9, 1)
 
     return placeholder
 end
@@ -15,13 +15,14 @@ function createScorePlaceholder(_display, _screenH, _screenW)
     placeholder = _display.newRect(0, 0, _screenW / 3, 100)
     placeholder.anchorX = 0
     placeholder.anchorY = 0
-    placeholder:setFillColor(0.3, 0.3, 0.9, 1)
+    placeholder:setFillColor(0.9,0.9,0.9, 1)
 
     return placeholder
 end
 
--- bubbleColorScore {"color" = count}
-function updateColorScore(_display, _bubbleColorScore, _colorTargets) 
+-- bubbleColorScore {"color" = count}; colorTargets 
+function updateColorScore(_display, _bubbleColorScore, _levelTarget)
+    
 end
 
 return {createGoalPlaceholder = createGoalPlaceholder, createScorePlaceholder = createScorePlaceholder }
